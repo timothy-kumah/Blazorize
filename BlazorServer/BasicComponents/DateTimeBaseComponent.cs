ac4a3c8bf6fd34b0adca4c81f01216c6e36c4ded
timothy-kumah <timothy.kumah@amalitech.com>
2024-04-10 08:28:02 +0000

    Component Inheritance

---
+++ BlazorServer/BasicComponents/DateTimeBaseComponent.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorServer.BasicComponents
+{
+    public class DateTimeBaseComponent:ComponentBase
+    {
+        public DateTime dateTime { get; set; }
+    }
+}
